52f59beb967026d87c44f13750ace92f0c0ec536
poojithagottipati <160849305+poojithagottipati@users.noreply.github.com>
2024-03-27 11:15:14 +0530

    Create Stringlength.c

---
+++ Stringlength.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int stringLength(char *str) {
+    int length = 0;
+    while (*str != '\0') {
+        length++;
+        str++;
+    }
+    return length;
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    int length = stringLength(str);
+    printf("Length of the string: %d\n", length);
+
+    return 0;
+}
+/*
+Enter a string: poojitha
+Length of the string: 8
+*/
